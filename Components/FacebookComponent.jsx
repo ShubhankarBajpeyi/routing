6f3742b3dbb3411dd1b6dc4bde3e8f2781022bba
Shubhankar Bajpeyi <anshubajpeyi@gmail.com>
2018-01-28 14:22:34 +0530

    Shubhankar: Added facebook as a different component, and passed props to main.

---
+++ Components/FacebookComponent.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as fb from '../Src/fbLogin.js';
+class FacebookComponent extends React.Component {
+    render() {
+        return(
+            <div>
+            <div id="overlay"></div>
+                    <div id="popup">
+                        <div id="status"></div>
+                        <div id="userData"></div>
+                        <a href="javascript:void(0);" className='btn btn-danger col-xs-2 pull-right' onClick={fb.fbLogout} id="fbLinkLogout">Logout</a>                    
+                    </div>
+                    <div id="statusLogout"></div>
+                    <a href="javascript:void(0);" className='btn btn-primary col-xs-2 pull-right' onClick={fb.fbLogin} id="fbLink">{this.props.myData}</a>
+            </div>
+        );
+    
+    }
+
+}
+export default FacebookComponent;
--- Pages/Page1Content.jsx
+++ Pages/Page1Content.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as common from '../Src/common.js';
-import * as fb from '../Src/fbLogin.js';
 import { Link } from 'react-router-dom';
+import Facebook from '../Components/FacebookComponent.jsx';
 
 class Page1 extends React.Component {
     render() {
@@ -11,14 +11,7 @@ class Page1 extends React.Component {
         common.loginWidget();
         return (
             <div className="row">
-                <div id="overlay"></div>
-                <div id="popup">
-                    <div id="status"></div>
-                    <div id="userData"></div>
-                    <a href="javascript:void(0);" className='btn btn-danger col-xs-2 pull-right' onClick={fb.fbLogout} id="fbLinkLogout">Logout</a>                    
-                </div>
-                <div id="statusLogout"></div>
-                <a href="javascript:void(0);" className='btn btn-primary col-xs-2 pull-right' onClick={fb.fbLogin} id="fbLink">{this.state.facebook}</a>
+                <Facebook myData = {this.state.facebook}/>
                 <div className="col-md-4 col-md-offset-4 text-center">
                     <div className="search-box">
                         <div className="caption">
